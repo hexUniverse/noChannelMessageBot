8bba01554f7cd9a84f4c67cfd3a9d2796c48b7d8
root <root@de-he1.sudo.host>
2023-11-23 17:32:13 +0800

    ✨ log to topic

--- bot/methods/logToChannel.py
+++ bot/methods/logToChannel.py
@@ -8,4 +8,4 @@ log: logging.Logger = logging.getLogger(__name__)
 
 class LogToChannel:
     async def log_to_channel(self, cli: Client, text: str):
-        await cli.send_message(os.getenv("TG_CHANNEL"), text)
+        await cli.send_message(os.getenv("TG_CHANNEL"), text, reply_to_message_id=os.getenv("TG_TOPIC"))
